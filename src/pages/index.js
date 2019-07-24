7a642187858745f4fc50de567152c07082b39246
Philip Krause <philipkrause7@gmail.com>
2019-07-24 18:36:54 -0400

    addedImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import labs from "../images/64labs.png"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -8,13 +8,23 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi 64 Labs</h1>
-    <p>Welcome to my first Gatsby site.</p>
-    <p>I am building this site to learn Gatsby</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img src={labs} />
+      <h1>Hi 64 Labs</h1>
+      <p>Welcome to my first Gatsby site.</p>
+      <p>I am building this site to learn Gatsby</p>
+
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+
+      <Link to="/page-2/">Go to page 2</Link>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
